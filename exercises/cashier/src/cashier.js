373b47fe434942ae1bf65ae3eee5c700fc3e7f01
collinadams <collinadams04@gmail.com>
2018-02-12 15:32:14 -0800

    Exercise 4

--- exercises/cashier/src/cashier.js
+++ exercises/cashier/src/cashier.js
@@ -1,3 +1,25 @@
 export function cashier() {
   // TODO: implement me
+  let cartItems = [];
+  return {
+    add({ name, qty = 1, price }) {
+      cartItems.push({ name, price, qty });
+      return this;
+    },
+    get length() {
+      let l = 0;
+      for (let i = 0; i < cartItems.length; i++) {
+        l += cartItems[i].qty;
+      }
+      return l;
+    },
+    get total() {
+      let tot = 0;
+      for (let i = 0; i < cartItems.length; i++) {
+        tot += (cartItems[i].qty * cartItems[i].price);
+      }
+      return tot;
+    },
+  };
 }
+
