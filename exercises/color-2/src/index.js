42a4116a55050345b3e08608bab99471f614c481
collinadams <collinadams04@gmail.com>
2018-02-12 15:04:41 -0800

    alt answer for exercise 2

--- exercises/color-2/src/index.js
+++ exercises/color-2/src/index.js
@@ -1,14 +1,14 @@
-import { hexToRgb, rgbToHex } from './color-utils';
+import { hexToRgb as h2rgb, rgbToHex as r2g } from './color-utils';
 
 export let color = { r: 255, g: 0, b: 0 };
 // define the hex property here
 
 Object.defineProperty(color, 'hex', {
   get() {
-    return rgbToHex(this.r, this.g, this.b);
+    return r2g(this.r, this.g, this.b);
   },
   set(hex) {
-    let { r, g, b } = hexToRgb(hex);
+    let { r, g, b } = h2rgb(hex);
     this.r = r;
     this.g = g;
     this.b = b;
