c8db384625f228dfc06198b58ed79bf0a05b60b0
collinadams <collinadams04@gmail.com>
2018-02-13 11:41:27 -0800

    Exercise 6

--- exercises/document/src/document.js
+++ exercises/document/src/document.js
@@ -8,6 +8,19 @@ export function Doc(doc = { version: CURRENT_DOC_VERSION, data: {} }) {
 Doc.prototype = {
   _loadDocument(docVersion, data) {
     // TODO: Update document based on version
+    let nameParts = (data.name || '').split(/\s/g);
+    switch (docVersion) {
+      case 1:
+        data.firstName = nameParts[0];
+        data.lastName =
+          nameParts.length > 1 ? nameParts[nameParts.length - 1] : '';
+        data.location = 'Earth';
+      case 2:
+        data.email = 'nobody@example.com';
+      case 3:
+        data.middleName = nameParts.slice(1, nameParts.length - 1).join(' ');
+      case 4:
+    }
     this._data = data;
   },
   get data() {
