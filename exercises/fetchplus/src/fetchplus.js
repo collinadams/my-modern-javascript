25cab5fdc6c9b70e242c0eb7198fb5a401c08378
collinadams <collinadams04@gmail.com>
2018-02-13 16:52:10 -0800

    fixed exercise 9

--- exercises/fetchplus/src/fetchplus.js
+++ exercises/fetchplus/src/fetchplus.js
@@ -7,12 +7,11 @@ const fetchCache = {};
 
 // Do not change this function signature or the tests will break
 export default function fetchPlus(url, options, fetch = _fetch) {
-  if (fetchCache[url]) {
-    return fetchCache[url];
-  } else {
-    let cachedPromise = fetch(url, options).then(response => {
+  let cachedPromise = fetchCache[url];
+  if (!cachedPromise)
+    cachedPromise = fetch(url, options).then(response => {
       return response.json();
     });
-    fetchCache[url] = cachedPromise;
-  }
+  fetchCache[url] = cachedPromise;
+  return cachedPromise;
 }
